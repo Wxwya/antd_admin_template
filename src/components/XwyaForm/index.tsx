5f697eba60bb542c6e61c5850a4cd15037da7c63
xwya <747273022@qq.com>
2024-07-11 15:33:56 +0800

    封装form

--- src/components/XwyaForm/index.tsx
+++ src/components/XwyaForm/index.tsx
@@ -1,26 +1,52 @@
 import { memo, useCallback } from 'react'
-import { Form, Input } from 'antd'
+import { Form, Input, Select, Checkbox, Radio, DatePicker, Cascader, Switch, InputNumber, Image, Upload } from 'antd'
+import { PlusOutlined } from '@ant-design/icons';
+const { RangePicker } = DatePicker;
+const getBase64 = (img: FileType, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
 const XwyaForm = (props) => {
-  const { itemlist } = props
+  const { itemlist,row=1 } = props
   const [form] = Form.useForm()
   const onFinish = () => {
     console.log('验证')
   }
-  const getType = useCallback((type: string) => { 
-      switch (type) {
-      case 'input':
-        return <Input />
-      default:
-        return <Input />
+  const onCustomRequest = (o:any) => { 
+    console.log(o);
+    
+  }
+  const getType = useCallback((p: any) => {
+    p.content = p.content || {}
+    const typeObj = {
+      input: <Input {...p.content} />,
+      select: <Select {...p.content} />,
+      check: <Checkbox.Group {...p.content} />,
+      radio: <Radio.Group {...p.content} />,
+      date: <DatePicker {...p.content} />,
+      range: <RangePicker {...p.content} />,
+      casc: <Cascader {...p.content} />,
+      switch: <Switch {...p.content} />,
+      number: <InputNumber {...p.content} />,
+      upload: (<Upload  listType="picture-card"  customRequest={onCustomRequest} showUploadList={false} >
+       <button style={{ border: 0, background: 'none' }} type="button">
+              <PlusOutlined />
+              <div style={{ marginTop: 8 }}>Upload</div>
+            </button>
+      </Upload>)
+    
     }
+    return typeObj[p.type]
   } ,[]) 
   return (
     <>
-      <Form name="basic" form={form} onFinish={onFinish} autoComplete="off">
+      <Form name="basic" form={form} onFinish={onFinish} initialValues={{fileList:[],images:[]}} autoComplete="off">
         <div className="flex gap-4 flex-wrap">
-          {itemlist.map((item) => (
-             <Form.Item<FieldType> label="Password" name="password" rules={[{ required: true, message: '请输入!' }]}>
-            {getType(item.type)}
+          {itemlist.map((p,index) => (
+            <Form.Item<FieldType> style={{width:`calc(100% / ${row} - 10px)`}} {...p.item} key={index} >
+            {getType(p)}
            </Form.Item>
           ))}
          
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,18 +2,70 @@ import { Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import XwyaForm from '@/components/XwyaForm';
 import { useMemo } from 'react';
+const options = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+const normFile = (e: any) => {
+  console.log('Upload event:', e);
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
 const Home = () => { 
   const navigate = useNavigate()
   const toPage = () => {
     navigate('/info/about')
   }
   const itemlist = useMemo(() => (
-    [{ type: "input", item: {label:"姓名",name:"name"},input:{} }]
+    [
+      { type: "input", item: { label: "姓名", name: "name" }, content: {} },
+      { type: "select", item: { label: "性别", name: "sex" }, content: { options: [{ value: 'jack', label: 'Jack' }, { value: 'lucy', label: 'Lucy' }], } },
+      { type: "check", item: { label: "多选", name: "dx" }, content: { options: [{ value: 'jack', label: 'Jack' }, { value: 'lucy', label: 'Lucy' }], } },
+      { type: "radio", item: { label: "单选", name: "dx1" }, content: { options: [{ value: 'jack', label: 'Jack' }, { value: 'lucy', label: 'Lucy' }], } },
+      { type: 'date', item: { label: "日期1", name: "date1" }, content: {} },
+      { type: 'range', item: { label: "日期2", name: "date2" }, content: {} },
+      { type: 'casc', item: { label: "多级", name: "dj" }, content: { options } },
+      { type: "switch", item: { label: "开关", name: "switch" }, content: {} },
+      { type: "number", item: { label: "数字", name: "number" } },
+      { type: "upload", item: { label: "图片上传", name: "images",valuePropName:"fileList",getValueFromEvent:normFile},content: {} }
+    ]
   ),[])
   return (<>
     <div className="h-10 py-2">
       <Button type="primary" onClick={toPage}>Home</Button>
-      <XwyaForm itemlist={itemlist} />
+      <XwyaForm itemlist={itemlist} row={6} />
     </div>
   </>)
 }
